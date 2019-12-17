297f64b922eb5be4b21f906a4b975bb91d4c37ef
zhangyongcan <volcano.dayarch@gmail.com>
2019-12-17 14:52:50 +0800

    组价开发

---
+++ module_1/src/main/java/debug/Module1Application.java
@@ -0,0 +1,11 @@
+package debug;
+
+import android.app.Application;
+
+public class Module1Application extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
