df828b9e81ced049f5e3d45800c9305979a1307a
zhangyongcan <volcano.dayarch@gmail.com>
2020-01-11 16:27:07 +0800

    SplishMainActivity调成到MainActivity 在BaseActivity中隐藏Actionbar

--- app/src/main/java/com/itg/mudulecode/MainActivity.java
+++ app/src/main/java/com/itg/mudulecode/MainActivity.java
@@ -1,19 +1,9 @@
 package com.itg.mudulecode;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Person;
-import android.os.Build;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
-import com.itg.annotionlib.SPI;
 import com.itg.common.base.BaseActivity;
-import com.itg.module_main.SplashActivity;
 import com.itg.mudulecode.databinding.ActivityMainBinding;
-import com.itg.mudulecode.test.Persion;
 
 public class MainActivity extends BaseActivity<ActivityMainBinding> {
 
@@ -35,9 +25,6 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
 
 
     private void progressAnnotation() {
-        Class<Persion> clzss = Persion.class;
-        if (clzss.isAnnotationPresent(SPI.class)) {
-            Log.e("MainActivity", "Persion你是用了SPI Annotation");
-        }
+
     }
 }
--- app/src/main/java/com/itg/mudulecode/test/Persion.java
+++
@@ -1,7 +0,0 @@
-package com.itg.mudulecode.test;
-
-import com.itg.annotionlib.SPI;
-
-@SPI(isDelete = true)
-public class Persion {
-}
