07e326550c8a52f3e3554d0c35f0fb2fefd08cb5
luozhiran <820185535@qq.com>
2020-01-11 14:07:24 +0800

    注解练习

--- app/src/main/java/com/itg/mudulecode/MainActivity.java
+++ app/src/main/java/com/itg/mudulecode/MainActivity.java
@@ -1,8 +1,15 @@
 package com.itg.mudulecode;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Person;
+import android.os.Build;
 import android.os.Bundle;
+
+import com.itg.annotionlib.SPI;
+import com.itg.module_main.SplashActivity;
+import com.itg.mudulecode.test.Persion;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    private void progressAnnotation(){
+        Class<Person> clzss = Person.class;
+        if (clzss.isAnnotationPresent(SPI.class)){
+        }
     }
 }
---
+++ app/src/main/java/com/itg/mudulecode/test/Persion.java
@@ -0,0 +1,7 @@
+package com.itg.mudulecode.test;
+
+import com.itg.annotionlib.SPI;
+
+@SPI(isDelete = true)
+public class Persion {
+}
